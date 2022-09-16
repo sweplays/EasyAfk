47c5bfbf337c22815afb46371b0715f7f6622ea4
Rasmus Svensk <sweplays@rasmus-desktop.localdomain>
2022-09-16 17:37:41 +0200

    first commit

---
+++ src/main/java/dev/sweplays/EasyAfk/EasyAfk.java
@@ -0,0 +1,14 @@
+package dev.sweplays.EasyAfk;
+
+import java.lang.System.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyAfk extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("Hello, Spigot!");
+    }
+}
